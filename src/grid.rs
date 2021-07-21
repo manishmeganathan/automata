399d59702db494aab016713ba47767f09de6a2e3
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-21 23:28:50 +0530

    Create grid.rs
    - Added a module grid.rs
    - Implemented the Grid struct that represents the simulation cell grid
    - Added a constructor and initialize method for Grid

---
+++ src/grid.rs
@@ -0,0 +1,57 @@
+use crate::cell::Cell;
+use ggez::graphics;
+
+// A struct that represents the cell grid
+#[derive(Debug)]
+pub struct Grid {
+    // Represents the 2D vector of grid cells
+    cellgrid: Option<Vec<Vec<Cell>>>,
+    // Represents the 2D rectangular bounds of the grid
+    dimensions: Option<graphics::Rect>,
+    // Represents the size of a single cell
+    size: f32,
+    blend_mode: graphics::BlendMode,
+}
+
+// Constructor implemntations for Grid
+impl Grid {
+    // A constructor function that creates a  
+    // null grid for a given cell size in pixels
+    pub fn new(size: f32) -> Self {
+        Self {
+            dimensions: None,
+            blend_mode: graphics::BlendMode::Add,
+            size,
+            cellgrid: None,
+        }
+    }
+
+    // A method of Grid that initializes the grid for a given rectangle dimensions.
+    pub fn initialize(&mut self, dimensions: graphics::Rect) {
+        // Calculate the number of rows and columns in the grid
+        let rows = dimensions.w / self.size;
+        let cols = dimensions.h / self.size;
+
+        // Create a new vector (represents rows)
+        let mut grid = Vec::new();
+
+        // Iterate for each row
+        for _ in 0..(rows as i32) {
+            // Create a new vector (represents columns)
+            let mut column = Vec::new();
+            // Iterate for each column
+            for _ in 0..(cols as i32) {
+                // Create a new cell and push it into the column vector
+                column.push(Cell::new());
+            }
+
+            // Push the column vector into the row vector (grid)
+            grid.push(column);
+        }
+
+        // Assign the initialized grid vector to the grid struct
+        self.cellgrid = Some(grid);
+        // Assign the dimensions to the grid struct
+        self.dimensions = Some(dimensions);
+    }
+}
