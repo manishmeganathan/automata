14a95ed7950083d0f91843d937515c4f6d5c214f
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-25 03:55:50 +0530

    Create BinaryCell
    - Added a file binarycell.rs in commons
    - Added an enum BinaryCell that represents a on/off grid cell
    - Implemented random generation constructors for balanced and skewed randomization
    - Implemented the clone trait

---
+++ src/commons/binarycell.rs
@@ -0,0 +1,78 @@
+use rand::Rng;
+
+/// An enum that represents the states of a binary cell.
+///
+/// Possibile states:
+/// - ``BinaryCell::Active`` <- represents the ON state
+/// - ``BinaryCell::Passive`` <- represents the OFF state
+#[derive(Debug, PartialEq)]
+pub enum BinaryCell {
+    // Represent the off state
+    Passive = 0,
+    // Represent the on state
+    Active = 1,
+}
+
+/// Implementation of constructors for BinaryCell
+impl BinaryCell {
+    /// A constructor function that generates a new BinaryCell with a balanced random state.
+    /// Balanced generation means that there is an equal probability for a cell to be passive or active.
+    pub fn balanced() -> Self {
+        // Randomly generate a number between 0 and 1.
+        let val = rand::thread_rng().gen_range(0..=1);
+
+        // Check the value of generated number.
+        match val {
+            // If generated number is 0, return a Passive cell.
+            0 => BinaryCell::Passive,
+            // If generated number is 1, return a Active cell.
+            _ => BinaryCell::Active,
+        }
+    }
+
+    /// A constructor function that generates a new BinaryCell with a skewed random state.
+    /// Skewed generation means that there is a higher probability for a cell to be a particular state.
+    ///
+    /// - @param *skew* is a bool that determines whether the skew state is passive or active.
+    /// - @param *bias* is an i8 that represents the degree of skew towards the skew state. 
+    ///
+    /// A bias of 1 is equivalent to a balanced state generation regardless of the skew value 
+    /// and a bias of 100 with a skew state of true, means the chance of a Passive cell is 1 in 100.
+    pub fn skewed(skew: bool, bias: i8) -> Self {
+        // Randomly generate a number between 0 and the given bias
+        let val = rand::thread_rng().gen_range(0..=bias);
+
+        // If the skew is true, skew towards the Active state. 
+        if skew {
+            // Check the value of generated number.
+            match val {
+                // If generated number is 0, return a Passive cell.
+                0 => BinaryCell::Passive,
+                // Otherwise return an Active cell.
+                _ => BinaryCell::Active,
+            }
+        }
+        // Otherwise, skew towards the Passive state. 
+        else {
+            match val {
+                // If generated number is 0, return an Active cell.
+                0 => BinaryCell::Active,
+                // Otherwise return a Passive cell.
+                _ => BinaryCell::Passive,
+            }
+        }
+    }
+}
+
+/// Implementation of the Clone trait for BinaryCell
+impl Clone for BinaryCell {
+    /// A method that returns a new BinaryCell with the same state as the current BinaryCell.
+    fn clone(&self) -> Self {
+        match self {
+            // Cloning a passive cell
+            BinaryCell::Passive => BinaryCell::Passive,
+            // Cloning an active cell
+            BinaryCell::Active => BinaryCell::Active,
+        }
+    }
+}
