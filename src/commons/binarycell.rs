8cde35adc4cdb8298e7b97843d244400a69bc8a6
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-25 12:59:54 +0530

    Implement SimCell trait for BinaryCell
    - Added an implementation of SimCell for BinaryCells
    - Refactored the skew constructor

--- src/commons/binarycell.rs
+++ src/commons/binarycell.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use crate::commons::simulables::SimCell;
 
 /// An enum that represents the states of a binary cell.
 ///
@@ -14,10 +15,10 @@ pub enum BinaryCell {
 }
 
 /// Implementation of constructors for BinaryCell
-impl BinaryCell {
+impl SimCell for BinaryCell {
     /// A constructor function that generates a new BinaryCell with a balanced random state.
     /// Balanced generation means that there is an equal probability for a cell to be passive or active.
-    pub fn balanced() -> Self {
+    fn balanced() -> Self {
         // Randomly generate a number between 0 and 1.
         let val = rand::thread_rng().gen_range(0..=1);
 
@@ -38,28 +39,30 @@ impl BinaryCell {
     ///
     /// A bias of 1 is equivalent to a balanced state generation regardless of the skew value 
     /// and a bias of 100 with a skew state of true, means the chance of a Passive cell is 1 in 100.
-    pub fn skewed(skew: bool, bias: i8) -> Self {
+    ///
+    /// Valid values for skew are "active" and "passive".
+    fn skewed(skew: &str, bias: i8) -> Self {
         // Randomly generate a number between 0 and the given bias
         let val = rand::thread_rng().gen_range(0..=bias);
 
-        // If the skew is true, skew towards the Active state. 
-        if skew {
-            // Check the value of generated number.
-            match val {
+        // Check the skew value
+        match skew {
+            // Skew towards the active state
+            "active" => match val {
                 // If generated number is 0, return a Passive cell.
                 0 => BinaryCell::Passive,
                 // Otherwise return an Active cell.
                 _ => BinaryCell::Active,
-            }
-        }
-        // Otherwise, skew towards the Passive state. 
-        else {
-            match val {
+            },
+            // Skew towards the passive state
+            "passive" => match val {
                 // If generated number is 0, return an Active cell.
                 0 => BinaryCell::Active,
                 // Otherwise return a Passive cell.
                 _ => BinaryCell::Passive,
             }
+            // Invalid skew value
+            _ => panic!("invalid skew value")
         }
     }
 }
