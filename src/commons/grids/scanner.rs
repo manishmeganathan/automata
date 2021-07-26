096cf457dcae75fa37ece54c6df6f98fd431f2b1
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-27 03:58:48 +0530

    Update docs for GridScanner

--- src/commons/grids/scanner.rs
+++ src/commons/grids/scanner.rs
@@ -1,11 +1,11 @@
 use crate::commons::grids::cellgrid::CellGrid;
 use crate::simulation::simulables::{SimCell, SimGrid};
 
-// A type alias for a cell on the grid and its position in that grid
-// Represents the x, y position and the cell at that position on the grid
+/// A type alias for a cell on the grid and its position in that grid
+/// Represents the x, y position and the cell at that position on the grid
 pub type GridCell<C> = (usize, usize, C);
 
-// A struct that represents a scanning iterator for any SimGrid
+/// A struct that represents a scanning iterator for a SimGrid
 pub struct GridScanner<T> where T: SimGrid {
     // The 2D vector of the grid being scanned
     pub gridvector: Vec<Vec<T::Cell>>,
@@ -15,12 +15,12 @@ pub struct GridScanner<T> where T: SimGrid {
     pub current_row: usize,
 }
 
-// Implementation of the Iterator trait for GridIterator
+/// Implementation of the Iterator trait for GridScanner
 impl<C: SimCell> Iterator for GridScanner<CellGrid<C>> {
-    // Iterator item
+    /// Define the iterator item type
     type Item = GridCell<C>;
 
-    // A method that advances the iterator to the next position in the Grid
+    /// A method that advances the iterator to the next position in the grid
     fn next(&mut self) -> Option<Self::Item> {
         // Retrieve the element based on the iterator's current position on the grid
         let element = match (self.current_column, self.current_row) {
