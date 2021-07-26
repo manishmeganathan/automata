034f68baf759e8f08b842f219dd61886b409bc43
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-27 03:58:36 +0530

    Update docs for CellGrid

--- src/commons/grids/cellgrid.rs
+++ src/commons/grids/cellgrid.rs
@@ -1,25 +1,26 @@
 use ggez::graphics;
-//use ggez::nalgebra as na;
 use crate::simulation::simulables::{SimCell, SimGrid};
 use crate::commons::grids::scanner::{GridCell, GridScanner};
 
-// A struct that represents a grid of cells
+/// A struct that represents a grid of generic cells.
+/// The generic cell type must implement the SimCell trait.
 #[derive(Debug)]
-pub struct CellGrid<T> where T:SimCell {
-    // Represents the size of a single cell
-    pub cellsize: f32,
-    // Represents the 2D vector of grid cells
+pub struct CellGrid<T> where T: SimCell {
+    /// Represents the size of a single cell
+    cellsize: f32,
+    /// Represents the 2D vector of grid cells
     pub vector: Option<Vec<Vec<T>>>,
-    // Represents the bounds of the grid
+    /// Represents the 2D bounds of the grid
     pub dimensions: Option<graphics::Rect>,
 }
 
-// Implementation of SimGrid<T> trait for Grid
+/// Implementation of the SimGrid trait for CellGrid
 impl<T: SimCell> SimGrid for CellGrid<T> {
+    /// Define the type of cell in the grid
     type Cell = T;
 
-    // A constructor function that creates a null grid for a given cell size in pixels.
-    // The height of the state banner is set to 50px.
+    /// A constructor method that creates a null grid.
+    /// Set the given cell size into the struct.
     fn new(cellsize: f32) -> Self {
         Self {
             cellsize,
@@ -28,17 +29,23 @@ impl<T: SimCell> SimGrid for CellGrid<T> {
         }
     }
 
-    // 
+    /// A setter method that sets the given grid vector to the struct.
     fn setgrid(&mut self, other: Vec<Vec<Self::Cell>>) {
         self.vector = Some(other);
     }
 
+    /// A setter method that sets the given grid dimensions to the struct.
     fn setdimensions(&mut self, other: graphics::Rect) {
         self.dimensions = Some(other);
     }
 }
 
+/// Implementation of builder methods for CellGrid.
+/// A collection of functions that build various intial states of the grid.
 impl<T: SimCell> CellGrid<T> {
+    /// A function that creates a randomized grid of cells for the
+    /// given cell size and grid dimensions. All cell states have
+    /// an equal probability of occuring on the grid.
     pub fn generate_randomgrid_balanced(cellsize: f32, dimensions:graphics::Rect) -> Vec<Vec<T>> {
         // Calculate the number of rows and columns in the grid
         let rows = dimensions.w / cellsize;
@@ -53,7 +60,8 @@ impl<T: SimCell> CellGrid<T> {
             let mut column = Vec::new();
             // Iterate for each column
             for _ in 0..(cols as i32) {
-                // Create a new cell and push it into the column vector
+                // Create a new cell with the balanced randomizer 
+                // and push it into the column vector
                 column.push(T::balanced());
             }
 
@@ -66,8 +74,10 @@ impl<T: SimCell> CellGrid<T> {
     }
 }
 
-// Implementation of the Clone trait for Grid
+/// Implementation of the Clone trait for Grid
 impl<T: SimCell> Clone for CellGrid<T> {
+    /// A method that returns a new CellGrid with the same  
+    /// vector and properties as the current CellGrid.
     fn clone(&self) -> Self {
         Self {
             cellsize: self.cellsize,
@@ -77,14 +87,14 @@ impl<T: SimCell> Clone for CellGrid<T> {
     }
 }
 
-// Implementation of the IntoIterator trait for Grid
+/// Implementation of the IntoIterator trait for Grid
 impl<T: SimCell> IntoIterator for CellGrid<T> {
-    // Defines the iteration item
+    /// Define the iteration item type
     type Item = GridCell<T>;
-    // Defines the iteration iterator
+    /// Define the iterator type
     type IntoIter = GridScanner<CellGrid<T>>;
 
-    // Defines the iterator's initial state
+    /// A method that creates an iterator for the CellGrid.
     fn into_iter(self) -> Self::IntoIter {
         // Check the option value
         match self.vector {
