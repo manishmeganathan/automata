4c860ac9c49b2ee979813e7a023099b102322c5d
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-27 03:59:53 +0530

    Update simulation event handler
    - Updated the automaton render function interface
    - Updated the automaton initialization interface.
    - Updated docs

--- src/simulation/simevents.rs
+++ src/simulation/simevents.rs
@@ -2,15 +2,15 @@ use ggez::timer;
 use ggez::event;
 use ggez::graphics;
 use ggez::GameResult;
-//use ggez::nalgebra as na;
+use ggez::nalgebra as na;
 
 use crate::simulation::simulables::{Automaton};
 use crate::simulation::simulation::Simulation;
 
-// Implementation of the EventHandler trait for Simulation
+/// Implementation of the EventHandler trait for Simulation
 impl<T: Automaton> event::EventHandler for Simulation<T> {
 
-    // A method that is called when the simulation state is to be updated
+    /// A method that is called when the simulation update is triggered by the event loop
     fn update(&mut self, ctx: &mut ggez::Context) -> GameResult<()> {
         // If FPS is set to 0, then no rate-limiting
         if self.fps == 0 {
@@ -30,7 +30,7 @@ impl<T: Automaton> event::EventHandler for Simulation<T> {
         Ok(())
     }
 
-    // A method that is called when the simulation the simulation is to be rendered
+    /// A method that is called when a render event is to be triggered by the event loop
     fn draw(&mut self, ctx: &mut ggez::Context) -> GameResult {
         // Clear the graphics window
         graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
@@ -40,18 +40,20 @@ impl<T: Automaton> event::EventHandler for Simulation<T> {
             // Retrieve the size of the graphics window
             let width = graphics::screen_coordinates(ctx).w;
             let height = graphics::screen_coordinates(ctx).h;
+            // Create rectangle dimensions
             let dimensions = graphics::Rect::new(0.0, 0.0, width, height);
 
-            // Initialize the grid
-            self.automaton.initialize("random", dimensions);
+            // Initialize the automaton with the screen dimensions
+            self.automaton.initialize(dimensions);
             // Set the initialized flag to true
             self.initialized = true;
         }
 
-        // Render the grid
-        //self.grid.draw(ctx, (na::Point2::new(0.0, 0.0),).into())?;
+        // Render the automaton state
+        self.automaton.draw(ctx, (na::Point2::new(0.0, 0.0),).into())?;
         // Present the graphics on the window
         graphics::present(ctx)?;
+        
         // Return an GameResult::Ok
         Ok(())
     }
