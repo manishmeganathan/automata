6d5d40a0591b8826e8c8c65a5bab227c4e14fd80
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-26 23:21:49 +0530

    Rename event.rs
    - Renamed event.rs to simevent.rs
    - Reworked for Automaton trait (wip)

--- src/simulation/simevents.rs
+++ src/simulation/simevents.rs
@@ -2,26 +2,27 @@ use ggez::timer;
 use ggez::event;
 use ggez::graphics;
 use ggez::GameResult;
-use ggez::nalgebra as na;
+//use ggez::nalgebra as na;
 
-use crate::commons::simulables::SimGrid;
-use crate::simulation::sim::Simulation;
+use crate::simulation::simulables::{Automaton};
+use crate::simulation::simulation::Simulation;
 
 // Implementation of the EventHandler trait for Simulation
-impl<T: SimGrid> event::EventHandler for Simulation<T> {
+impl<T: Automaton> event::EventHandler for Simulation<T> {
+
     // A method that is called when the simulation state is to be updated
     fn update(&mut self, ctx: &mut ggez::Context) -> GameResult<()> {
         // If FPS is set to 0, then no rate-limiting
         if self.fps == 0 {
-            // Update the simulation state
-            self.grid.update();
+            // Advance the automaton state
+            self.automaton.advance();
         
         // Otherwise refresh the graphics with the set FPS rate
         } else {
             // Wait for the FPS time to elapse
             while timer::check_update_time(ctx, self.fps) {
-                // Update the simulation state
-                self.grid.update();
+                // Advance the automaton state
+                self.automaton.advance();
             }
         }
 
@@ -34,20 +35,21 @@ impl<T: SimGrid> event::EventHandler for Simulation<T> {
         // Clear the graphics window
         graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
 
-        // Retrieve the size of the graphics window
-        let width = graphics::screen_coordinates(ctx).w;
-        let height = graphics::screen_coordinates(ctx).h;
-
-        // Check if the grid has been initialized
+        // Check if the simulation has been initialized
         if !self.initialized {
+            // Retrieve the size of the graphics window
+            let width = graphics::screen_coordinates(ctx).w;
+            let height = graphics::screen_coordinates(ctx).h;
+            let dimensions = graphics::Rect::new(0.0, 0.0, width, height);
+
             // Initialize the grid
-            self.grid.initialize(graphics::Rect::new(0.0, 0.0, width, height));
+            self.automaton.initialize("random", dimensions);
             // Set the initialized flag to true
             self.initialized = true;
         }
 
         // Render the grid
-        self.grid.draw(ctx, (na::Point2::new(0.0, 0.0),).into())?;
+        //self.grid.draw(ctx, (na::Point2::new(0.0, 0.0),).into())?;
         // Present the graphics on the window
         graphics::present(ctx)?;
         // Return an GameResult::Ok
