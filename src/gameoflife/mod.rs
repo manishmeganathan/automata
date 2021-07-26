cabcf7668d5468c8287cc59de1728aaf514b953b
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-27 04:01:03 +0530

    Add GameOfLife struct
    - Added a generic GameOfLife struct in gameoflife/mod.rs

--- src/gameoflife/mod.rs
+++ src/gameoflife/mod.rs
@@ -1,2 +1,20 @@
-pub mod iter;
-pub mod grid;
+mod cellgrid;
+
+use crate::simulation::simulables::SimGrid;
+
+/// A struct that represents the automaton for 
+// Conway's Game of Life on a generic grid.
+pub struct GameOfLife<T> where T: SimGrid {
+    /// Represents the automaton grid.
+    grid: T,    
+    /// Represents the size of an individual cell.
+    cellsize: f32,
+    /// Represents the intial state of the automaton.
+    initialstate: String,
+    /// Represents the number of times the grid has been updated
+    generation: u32,
+    /// Represents the number of cells that are alive
+    alive: u32,
+    /// Represents the number of cells that are dead
+    dead: u32,
+}
