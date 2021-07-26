1f987687a8cf09e99cb76fa0198b0564ee428166
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-27 04:00:17 +0530

    Update docs for Simulation

--- src/simulation/simulation.rs
+++ src/simulation/simulation.rs
@@ -1,28 +1,25 @@
 use ggez::GameResult;
 use crate::simulation::simulables::{Automaton};
 
-// A generic struct that represents a simulation
+/// A struct that represents the simulator for a generic automaton.
 pub struct Simulation<T> where T: Automaton {
-    // Represents the automaton to simulate
+    /// Represents the automaton that is being simulated.
     pub automaton: T,
-    // Represents the refresh rate of the simulation 
+    /// Represents the refresh rate of the simulation.
     pub fps: u32,
-    pub cellsize: f32,
-    // Represents whether the automaton has been initialized
+    /// Represents whether the simulation has been initialized.
     pub initialized: bool,
 }
 
-// Constructor implementation for Simulation
+/// Implementation of constructor for Simulation.
 impl<T: Automaton> Simulation<T> {
-    // A constructor function that creates a new simulation with
-    // the given cell size (pixels) and refresh rate (seconds).
+    /// A constructor function that creates a new simulation automaton with
+    /// the given intial state, cell size (pixels) and refresh rate (seconds).
     pub fn new(initialstate: &str, cellsize: f32, fps: u32) -> GameResult<Self> {
-        Ok(Self {
-            fps,
-            cellsize,
+        // Create the simulation, wrap it in a GameResult and return it.
+        Ok(Self{
+            fps, initialized: false,
             automaton: T::new(initialstate, cellsize), 
-            initialized: false,
         })
     }
-
 }
