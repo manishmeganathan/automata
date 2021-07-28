33aa37c26d4953f17af749f919bb8ea8dc7f5234
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-28 16:53:06 +0530

    Update simulable traits
    - Refactored imports
    - Added the Copy trait requirement for SimCell
    - Added a trait type Orientation that must implement the Orient trait for SimGrid
    - Added getter methods for height and width of grid in SimGrid
    - Added a random cell retrieval for SimGrid
    - Added fullname method for Automaton

--- src/simulation/simulables.rs
+++ src/simulation/simulables.rs
@@ -1,8 +1,10 @@
 use ggez::graphics;
+use crate::commons::grids::GridCell;
+use crate::commons::navigation::Orient;
 
 /// A trait for a simulation cell
 /// The cell must be cloneable
-pub trait SimCell: Clone {
+pub trait SimCell: Clone + Copy {
     /// A constructor method that generates a random 
     /// new cell with a balanced ratio.
     fn balanced() -> Self;
@@ -17,6 +19,9 @@ pub trait SimCell: Clone {
 pub trait SimGrid: Clone + IntoIterator {
     /// Defines the type of cell used in the grid.
     type Cell: SimCell;
+    
+    /// Defines the type of navigation used in the grid.
+    type Orientation: Orient; 
 
     /// A constructor method that create a null grid.
     fn new(cellsize: f32) -> Self;
@@ -26,6 +31,15 @@ pub trait SimGrid: Clone + IntoIterator {
 
     /// A setter method that sets the grid dimensions.
     fn setdimensions(&mut self, dimensions: graphics::Rect);
+
+    /// A getter method that returns the grid vector's height (no.of rows).
+    fn getheight(&self) -> usize;
+
+    /// A getter method that returns the grid vector's width (no.of columns).
+    fn getwidth(&self) -> usize;
+
+    /// A method that returns a random cell from the grid.
+    fn randomcell(&self) -> GridCell<Self::Cell>;
 }
 
 /// A trait for simulation automaton.
@@ -45,11 +59,16 @@ pub trait Automaton: graphics::Drawable {
     /// A method that advances the automaton to the next generation.
     fn advance(&mut self);
 
-    /// A method that returns the name of the automaton along
-    /// with its initial state and grid type as formatted string.
-    fn name(&self) -> String;
-
     /// A method that returns the current state of the 
     /// automaton as a formatted string.
-    fn state(&self) -> String;   
+    fn state(&self) -> String;  
+
+    /// A method that returns the name of the automaton as a string.
+    /// Ex: "Conway's Game of Life"
+    fn name(&self) -> String;
+
+    /// A method that returns the name of the automaton along
+    /// with its initial state and grid type as formatted string.
+    /// Ex: "Conway's Game of Life | Grid | Random [1:1]"
+    fn fullname(&self) -> String;    
 }
