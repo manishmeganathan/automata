840a03021ca3e69f835ef470dab91d3a1bdd0a88
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-28 17:02:15 +0530

    Implement automaton initialization for langtons ant
    - Added the intialize method for langton's ant on CellGrid

--- src/langtonsant/cellgrid.rs
+++ src/langtonsant/cellgrid.rs
@@ -1,7 +1,10 @@
+use ggez::graphics;
+
 use crate::langtonsant::LangtonsAnt;
 use crate::commons::grids::CellGrid;
 use crate::commons::cells::BinaryCell;
 use crate::simulation::{SimGrid, Automaton};
+use crate::commons::navigation::{Turmite, Orient, Direction4};
 
 
 /// Implementation of the Automaton trait for Langton's Ant with a CellGrid grid,
@@ -18,6 +21,37 @@ impl Automaton for LangtonsAnt<CellGrid<BinaryCell>> {
             cellsize,
             generation: 0,
             ant: None,
+        }
+    }
+
+    /// A method that initializes the automaton for the given dimensions.
+    fn initialize(&mut self, dimensions: graphics::Rect) {
+        // Create a new dimensions object for the grid of cells (60 px removed for the banner)
+        let griddimensions = graphics::Rect::new(0.0, 0.0, dimensions.w, dimensions.h - 60.0);
+        // Set the grid dimensions to the grid 
+        self.grid.setdimensions(griddimensions);
+        
+        // Check the value of the initial state field
+        match self.initialstate.as_str() {
+            // Default initial state (random-balanced)
+            "default" => {
+                // Set the initial state string of the automaton
+                self.initialstate = "Empty".to_string();
+
+                // Create a grid of random cells with a balanced ratio of dead and alive cells
+                let randomgrid = CellGrid::<BinaryCell>::generate_empty_grid(self.cellsize, griddimensions);
+                // Set the generated grid to the automaton grid
+                self.grid.setgrid(randomgrid);
+
+                self.ant = Some(Turmite::new(&self.grid));
+            },
+
+            // Invalid initial state
+            _ => {
+                // Print an error and exit
+                eprintln!("[error] invalid initial state for 'gameoflife'");
+                std::process::exit(0);
+            }
         }
     }
 
